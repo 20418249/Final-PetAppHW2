6dc270fe3e1caec2bd3ce7833116410b6ebfdf52
Christiaan Holm Tuks <u20418249@tuks.co.za>
2023-10-06 12:42:22 +0200

    Stable Views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,27 +1,58 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PetAppHW2.Data;
 using PetAppHW2.Models;
+using System;
 using System.Diagnostics;
+using System.IO;
+using static System.Net.WebRequestMethods;
 
 namespace PetAppHW2.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _db; //declare a instance of the database
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
         {
             _logger = logger;
+            _db = db; //set the database
         }
 
-        public IActionResult Index()
-        {
-            return View();
+
+        //Use async for the data to load from the database first
+        public async Task<IActionResult> Index()
+        { 
+            //Create a pets list
+            List<Pet> pets = await _db.Pets.ToListAsync();
+            
+            //Counter for the number of pets that got addopted.
+            //------------------------------------------------
+            int i = 0;
+            foreach (var item in pets)
+            {
+                if (item.userId !=  null)
+                {
+                    i++;
+                }    
+                ViewBag.noAdopted = i;
+            }
+            //------------------------------------------------
+
+            return View(pets);
         }
+
+
+
+
 
         public IActionResult Privacy()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
