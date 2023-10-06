d6d5ea65b9fe617a270fbc78fb3c549edbb84a67
Christiaan Holm Tuks <u20418249@tuks.co.za>
2023-10-06 15:09:14 +0200

    Latest Working Model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace PetAppHW2.Controllers
         { 
             //Create a pets list
             List<Pet> pets = await _db.Pets.ToListAsync();
-            
+            List<User> user = await _db.users.ToListAsync();
             //Counter for the number of pets that got addopted.
             //------------------------------------------------
             int i = 0;
@@ -41,11 +41,24 @@ namespace PetAppHW2.Controllers
             }
             //------------------------------------------------
 
+
             return View(pets);
         }
 
 
-
+        public async Task<IActionResult> ViewPets(string petType = "", string petBreed = "", string Location = "", string action = "")
+        {
+            List<Pet> pets = new List<Pet>();
+            if (action == "2" || action == "")
+            {
+                pets = await _db.Pets.ToListAsync();
+            }
+            else
+            {
+                pets = await _db.Pets.Where(x => x.PetType.description == petType && x.PetBreed.description == petBreed && x.Location.name == Location).ToListAsync();
+            }
+            return View(pets);
+        }
 
 
         public IActionResult Privacy()
---
+++ ViewModels/ViewPetsVM.cs
@@ -0,0 +1,20 @@
+﻿namespace PetAppHW2.ViewModels
+{
+    public class ViewPetsVM
+    {
+    
+    
+        public int PetId { get; set; }
+        public int petTypeId { get; set; }
+        public int petBreedId { get; set; }
+        public int locationId { get; set; }
+        public int genderId { get; set; }
+        public string PetName { get; set; }
+        public string PetType { get; set; }
+        public string PetBreed { get; set; }
+        public string Location { get; set; }
+        public string UserName { get; set; }
+    
+    
+    }
+}
